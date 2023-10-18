fd1aacd78bc3753f96ee9d4bbaa46b73e5deb8b3
jorgem9922 <jorgemiracerda@gmail.com>
2023-10-18 18:38:25 +0200

    ddd

---
+++ www/ejercicio1.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Welcome to LAMP Infrastructure</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div>
+            <?php
+             
+
+             
+            ?>
+        </div>
+    </body>
+</html>
