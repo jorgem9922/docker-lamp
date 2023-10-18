d8af98db55e21e7c30177f6f5ca6de3175a6c61d
jorgem9922 <jorgemiracerda@gmail.com>
2023-10-18 20:36:51 +0200

    upload

--- www/00_ini/ejer1.1b.php
+++ www/00_ini/ejer1.1b.php
@@ -10,19 +10,18 @@
         <h4>Actualice la pagina para mostrar una linea nueva<h4>
             
    
-<svg height="2100px" width="1000px">
+<svg height="100%" width="100%">
 
 <?php
 
-$posx2=rand(10,1000);
-if ($posx2<1000)
+$posx21=rand(10,1000);
+if ($posx21<1000)
 {
-    echo '<line x1="0" y1="100" x2="'. $posx2 .'" y2="100" style="stroke:rgb(0,125,125);stroke-width:3" />';
+    echo '<line x1="0" y1="100" x2="'. $posx21 .'" y2="100" style="stroke:rgb(0,125,125);stroke-width:3" />';
     
 }
 ?>
+
 </svg>
-
-
 </body>
 </html>
---
+++ www/00_ini/ejer1.2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+// $i=1;
+// while ($i<){
+$dado1=rand(1,6);
+$dado2=rand(1,6);
+echo '<img src="/Recursos/' . $dado2 . '.svg " alt="">';
+
+echo '<img src="/Recursos/' . $dado1 . '.svg " alt="">';
+
+if($dado2<$dado1){
+    echo 'dado 1 vence a dado--2';
+}
+elseif ($dado2>$dado1){
+echo 'dado 2 vence a dado 1';
+}
+else{
+echo'empate';
+
+}
+// $i++;
+// }
+?>
+
+<!-- <svg height="500" width="1000"> -->
+
+
+<?php
+?>
+</svg>
+
+</body>
+</html>
