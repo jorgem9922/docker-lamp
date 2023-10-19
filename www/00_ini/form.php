7109454078e070c40d8a057bd1ff9a1734d75b19
jorgem9922 <jorgemiracerda@gmail.com>
2023-10-19 11:27:44 +0200

    1127

--- www/00_ini/form.php
+++ www/00_ini/form.php
@@ -1,8 +1,9 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?><BR>
+Your city is <?php echo $_GET["city"]; ?>
 
 </body>
 </html>
