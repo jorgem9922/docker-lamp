8e37445529a2ebb03008b48e9c18c5d7ff98a81d
jorgem9922 <jorgemiracerda@gmail.com>
2023-10-21 13:10:09 +0200

    hjky

--- www/03_svg.php
+++ www/03_svg.php
@@ -17,11 +17,7 @@ echo '<img src="/Recursos/' . $dado2 . '.svg " alt="">';
 echo '<img src="/Recursos/' . $dado1 . '.svg " alt="">';
 echo 'la suma de los dados es: ' .$dado1+$dado2;
 if($dado2<$dado1){
-<<<<<<< HEAD
-    echo 'dado 1 vence a dado 2';
-=======
     echo 'dado 1 vence a dado--2';
->>>>>>> d8af98db55e21e7c30177f6f5ca6de3175a6c61d
 }
 elseif ($dado2>$dado1){
 echo 'dado 2 vence a dado 1';
